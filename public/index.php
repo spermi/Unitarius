c30aecfd6244d9f20dfe9d1ae809e2527ff6691d
Spermi <kovacszsoltsp@gmail.com>
2025-09-21 21:04:46 +0300

    Fixed case sensitive load on Helpers.php

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../src/Core/helpers.php';
+require __DIR__ . '/../src/Core/Helpers.php';
 
 use Core\ErrorHandler;
 use Core\Router;
